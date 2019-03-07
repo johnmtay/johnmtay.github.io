aa682a1e31adef3bd637931c9471f62f9ef15dd3
johnmtay <tay18008@byui.edu>
2019-03-07 16:34:37 -0700

    added homepage and copied previous pages

---
+++ cit230/lesson-9/scripts/home.js
@@ -0,0 +1,28 @@
+var requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+var request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+
+request.onload = function(){
+    var data = resquest.response;
+    printData(data);
+}
+
+function printData(jsonObj){
+    var data = jsonObj['towns']
+    document.getElementById('prestonMotto').innerHTML = townData.towns[4].motto;
+    document.getElementById('prestonYearFounded').innerHTML = townData.towns[4].yearFounded;
+    document.getElementById('prestonPopulation').innerHTML = townData.towns[4].currentPopulation;
+    document.getElementById('prestonRainFall').innerHTML = townData.towns[4].averageRainfall;
+
+    document.getElementById('sodaSpringsMotto').innerHTML = townData.towns[5].motto;
+    document.getElementById('sodaSpringsYearFounded').innerHTML = townData.towns[5].yearFounded;
+    document.getElementById('sodaSpringsPopulation').innerHTML = townData.towns[5].currentPopulation;
+    document.getElementById('sodaSpringsRainFall').innerHTML = townData.towns[5].averageRainfall;
+
+    document.getElementById('fishHavenMotto').innerHTML = townData.towns[1].motto;
+    document.getElementById('fishHavenYearFounded').innerHTML = townData.towns[1].yearFounded;
+    document.getElementById('fishHavenPopulation').innerHTML = townData.towns[1].currentPopulation;
+    document.getElementById('fishHavenRainFall').innerHTML = townData.towns[1].averageRainfall;
+}
