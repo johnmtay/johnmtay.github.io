323ad2b2709e8bbcba929d66c865d806f5827b1f
johnmtay <tay18008@byui.edu>
2019-04-12 18:31:35 -0600

    updated temples and JS

--- cit230/final/scripts/temples.js
+++ cit230/final/scripts/temples.js
@@ -1,28 +1,37 @@
-var requestURL = 'https://johnmtay.github.io/cit230/final/scripts/closures.json';
+var requestURL = 'https://johnmtay.github.io/cit230/final/scripts/templeData.json';
 var request = new XMLHttpRequest();
 request.open('GET', requestURL);
 request.responseType = 'json';
 request.send();
 
 request.onload = function() {
-    var townData = request.response;
-    showData(townData);
+    var templeData = request.response;
+    showData(templeData);
 }
 
-function showData(townData) {
-    //var data = jsonObj['towns']
-    document.getElementById('prestonMotto').innerHTML += townData.towns[4].motto;
-    document.getElementById('prestonYearFounded').innerHTML += townData.towns[4].yearFounded;
-    document.getElementById('prestonPopulation').innerHTML += townData.towns[4].currentPopulation;
-    document.getElementById('prestonRainFall').innerHTML += townData.towns[4].averageRainfall;
+function showData(templeData) {
+    //var data = jsonObj['temples']
+    document.getElementById('gilbertAddress').innerHTML += templeData.temples[1].address;
+    document.getElementById('gilbertPhone').innerHTML += templeData.temples[1].phone;
+    document.getElementById('gilbertDedication').innerHTML += templeData.temples[1].dedication;
+    document.getElementById('gilbertServices').innerHTML += templeData.towns[1].services;
+    document.getElementById('gilbertClosures').innerHTML += templeData.towns[1].closures;
 
-    document.getElementById('sodaSpringsMotto').innerHTML += townData.towns[5].motto;
-    document.getElementById('sodaSpringsYearFounded').innerHTML += townData.towns[5].yearFounded;
-    document.getElementById('sodaSpringsPopulation').innerHTML += townData.towns[5].currentPopulation;
-    document.getElementById('sodaSpringsRainFall').innerHTML += townData.towns[5].averageRainfall;
+    document.getElementById('konaAddress').innerHTML += templeData.temples[2].address;
+    document.getElementById('konaPhone').innerHTML += templeData.temples[2].phone;
+    document.getElementById('konaDedication').innerHTML += templeData.temples[2].dedication;
+    document.getElementById('konaServices').innerHTML += templeData.towns[2].services;
+    document.getElementById('konaClosures').innerHTML += templeData.towns[2].closures;
 
-    document.getElementById('fishHavenMotto').innerHTML += townData.towns[1].motto;
-    document.getElementById('fishHavenYearFounded').innerHTML += townData.towns[1].yearFounded;
-    document.getElementById('fishHavenPopulation').innerHTML += townData.towns[1].currentPopulation;
-    document.getElementById('fishHavenRainFall').innerHTML += townData.towns[1].averageRainfall;
+    document.getElementById('newportAddress').innerHTML += templeData.temples[3].address;
+    document.getElementById('newportPhone').innerHTML += templeData.temples[3].phone;
+    document.getElementById('newportDedication').innerHTML += templeData.temples[3].dedication;
+    document.getElementById('newportServices').innerHTML += templeData.towns[3].services;
+    document.getElementById('newportClosures').innerHTML += templeData.towns[3].closures;
+
+    document.getElementById('oquirrhAddress').innerHTML += templeData.temples[4].address;
+    document.getElementById('oquirrhPhone').innerHTML += templeData.temples[4].phone;
+    document.getElementById('oquirrhDedication').innerHTML += templeData.temples[4].dedication;
+    document.getElementById('oquirrhServices').innerHTML += templeData.towns[4].services;
+    document.getElementById('oquirrhClosures').innerHTML += templeData.towns[4].closures;
 }
